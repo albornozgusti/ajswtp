4914d338508caaf2c5d6fb28b2c096e610af4c43
Gustavo Albornoz <albornozgusti@gmail.com>
2022-10-02 16:35:16 -0300

    initial commit for ajsw

---
+++ src/main/java/com/example/ajswtp/AjswtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajswtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjswtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjswtpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ajswtp/AjswtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajswtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjswtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
