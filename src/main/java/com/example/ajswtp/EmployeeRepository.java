5189b3031cd926d80d5170483d4ea52889f32786
Gustavo Albornoz <albornozgusti@gmail.com>
2022-10-03 17:30:52 -0300

    feat: add functions on interface EmployeeRepository

--- src/main/java/com/example/ajswtp/EmployeeRepository.java
+++ src/main/java/com/example/ajswtp/EmployeeRepository.java
@@ -1,4 +1,11 @@
 package com.example.ajswtp;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("employeeRepository")
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Employee findById(Long id);
+    Employee replaceEmployee(Employee newEmployee, Long id);
 }
